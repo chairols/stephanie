19da8671ae8c6b32219c7b37a6f0b479e60be5e8
Hernan <Hernan@Hernan-PC>
2014-08-07 10:59:40 -0300

    - Se quitó el frontend anterio y se puso uno nuevo

---
+++ stephanie/application/controllers/fotos.php
@@ -0,0 +1,12 @@
+<?php
+
+class Fotos extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function index() {
+        
+    }
+}
+?>
